d4d0f75974409dc2e24caf0a84afd3aafea8d00f
Mahmoud Ali Mahmoud <mah01023555113@gmail.com>
2026-07-07 13:32:57 +0300

    Add comments

--- NXT25_AST4_SWD5_S2-InGym/Models/GymMember.cs
+++ NXT25_AST4_SWD5_S2-InGym/Models/GymMember.cs
@@ -2,6 +2,7 @@
 {
     public class GymMember
     {
+        //in class GymMember, we have two properties: GymID and MemberID, which are both of type int. These properties represent the foreign keys that link the GymMember entity to the Gym and Member entities, respectively.
         public int GymID { get; set; }
 
         public int MemberID { get; set; }
